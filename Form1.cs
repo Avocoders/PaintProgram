1673d464354babaeb4f5734b674f1839937c9fbf
serg <s.401@mail.ru>
2020-04-10 20:37:36 +0300

    Critical changes

--- Form1.cs
+++ Form1.cs
@@ -7,34 +7,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp7.Interface;
 
 namespace WindowsFormsApp7
 {
     public partial class Form1 : Form
-    {
+    {   
+        Bitmap q;
+        IFigure figure;
+        public  Color color = Color.Red;
+        bool isDrow;
         public Form1()
         {
             InitializeComponent();
         }
+        public Color GetColor()
+        {
+            return color;
+        }
 
 
-        Bitmap q;
         private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
         {
-            q.SetPixel(e.X, e.Y, Color.Red);
-            pictureBox1.Image = q;
+            if (isDrow == true)
+            {
+                q = figure.DrawFigure(e.X, e.Y, color);
+                pictureBox1.Image = q;
+            }
         }
 
         private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
         {
-           
+            isDrow = true;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            q = new Bitmap(pictureBox1.Width, pictureBox1.Height);
             
+            figure = new Line(pictureBox1.Width, pictureBox1.Height);
+            isDrow = false;
+        }
 
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDrow = false;
         }
     }
 }
---
+++ Interface/IFigure.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp7.Interface
+{
+    interface IFigure
+    {
+        Bitmap DrawFigure(int x, int y, Color color);
+    }
+}
