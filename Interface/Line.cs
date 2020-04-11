a7ca9344b999e2f52ef35a89e05df84a92cbc71e
serg <s.401@mail.ru>
2020-04-11 22:27:39 +0300

    add some err

--- Interface/Line.cs
+++ Interface/Line.cs
@@ -17,13 +17,13 @@ namespace WindowsFormsApp7.Interface
             Width = x;
             Height = y;
             bitmap = new Bitmap(Width, Height);
-            lastY = -1;
-            lastX = -1;
+            lastY = 0;
+            lastX = 0;
         }
         public Bitmap DrawFigure(int x, int y, Color color)
-        {   if ((lastX==x&&lastY==y)||(lastX==1&&lastY==1)||(lastX==-1&&lastY==-1))
+        {   if ((lastX==x&&lastY==y)||(lastX==1&&lastY==1)||(lastX==0&&lastY==0))
             { lastY = y; lastX = x; bitmap.SetPixel(x, y, color); }
-            else if(lastX!=x)
+            else if(lastX!=x&&lastY==y)
             {
                 double k = (y - lastY) / (x - lastX);
                 double b = lastY - k * lastX;
@@ -40,7 +40,7 @@ namespace WindowsFormsApp7.Interface
                 lastX = x;
                 lastY = y;
             }
-        else if(lastY!=y)
+        else if(lastY!=y&&lastX==x)
             {
                 double k;
                 if (x != lastX)
@@ -78,6 +78,39 @@ namespace WindowsFormsApp7.Interface
                 }
                 lastX = x;
                 lastY = y;
+            }
+        else if((lastX!=x&&lastY!=y) && (lastX != 1 && lastY != 1) &&(lastX != 0 && lastY != 0))
+            {
+                double c = lastX - x;
+                int i;
+                if(c>0)
+                { i = 1; }
+                else
+                { i = -1; }
+                double b =lastY - y;
+                int j;
+                if (b > 0)
+                { j = 1; }
+                else
+                { j = -1; }
+                if (Math.Abs(c) > Math.Abs(b))
+                {
+                    while(lastX!=x)
+                    {
+                        bitmap.SetPixel(x, y, color);
+                        x += i;
+                        y +=j * System.Convert.ToInt32(b / c);
+                    }
+                }
+                else
+                {
+                    while(lastY!=y)
+                    {
+                        bitmap.SetPixel(x, y, color);
+                        y += j;
+                        x = i * System.Convert.ToInt32(c / b);
+                    }
+                }
             }
             //bitmap.SetPixel(x, y,color);
             return bitmap;
