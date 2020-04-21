f6f148e52841e6c58827a10d5938fec94b164460
Slava <zaharko@yandex.ru>
2020-04-21 14:06:44 +0300

    strateg

---
+++ Drower.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp7
+{
+    public abstract class Drower
+    {
+        
+        public Brush brush;
+        public IFigur figure;
+        public Drower(Brush brush, IFigur figur) 
+        {
+            this.brush = brush;
+            this.figure = figur;
+        }
+        public abstract void Draw(Point point1, Point point2);
+    }
+}
--- Figure/StraightLine.cs
+++ Figure/StraightLine.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ namespace WindowsFormsApp7.Figure
     class StraightLine : IFigur
     {
         Brush q;
-
+        List<Point> square;
         public StraightLine(Brush bruch)
         {
             q = bruch;
@@ -17,7 +18,9 @@ namespace WindowsFormsApp7.Figure
 
         public void Drow(int x1, int y1, int x2, int y2,int nAngle)
         {
-            q.DrawLine(x1, y1, x2, y2);
+            //q.DrawLine(x1, y1, x2, y2);
+            square.Add(new Point(x1, y1));
+            square.Add(new Point(x2, y2));
         }
     }
 }
