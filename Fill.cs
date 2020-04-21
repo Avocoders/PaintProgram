f2355a41331b0c95b5f3dd74f5fa283df80fd31f
Veronika Belova <belovaveronika14@gmail.com>
2020-04-21 11:55:43 +0300

    Continue fill create

--- Fill.cs
+++ Fill.cs
@@ -12,14 +12,38 @@ namespace WindowsFormsApp7
         int x, y;
         SingleBitmap q = SingleBitmap.Create();
         Color color = Color.Black;
+
         public Fill(int x, int y)
         {
             this.x = x;
             this.y = y;
         }
-        public void Casting(int x, int y)
+        public void Casting(int x, int y, Color fillColor)
         {
-            
+            Color startColor = q.bitmap.GetPixel(x,y);
+            int leftX = x;
+            int rightX = x;
+            while(q.bitmap.GetPixel(leftX-1, y)== startColor && leftX>0)
+            {
+                leftX--;
+            }   
+            for(int i = leftX; i<=rightX; i++)
+            {
+                q.bitmap.SetPixel(i, y,fillColor);
+            }
+
+            for (int i = leftX; i <= rightX; i++)
+            {
+                if (q.bitmap.GetPixel(i, y - 1) == startColor && y>0)
+                {
+                    Casting(i, y - 1, fillColor); 
+                }
+                if (q.bitmap.GetPixel(i, y + 1) == startColor && y > 0)
+                {
+                    Casting(i, y - 1, fillColor);
+                }
+            }
+
         }
     }
 }
