006bd48c8d5168caf57e0469fd92d07f7d5d32dc
Veronika Belova <belovaveronika14@gmail.com>
2020-04-22 20:42:02 +0300

    Create DrawerFabric with child-classes

---
+++ DrawerFabric/FigureFabric.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp7.DrawerFabric
+{
+    public class FigureFabric : AbstractFabric
+    {
+        public override Drower CreateDrower(IFigur selectedFigur)
+        {
+            Brush brush = new Brush(); // selectedColor, selectedSize
+            abstractDrower = new ClassFigure(brush, selectedFigur, null); // IFill fill
+            return abstractDrower;
+        }
+    }
+}
